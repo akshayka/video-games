64491e14076caa0d7b3458107b01294b0d5d9523
Akshay <akshaykagrawal7@gmail.com>
2013-08-24 12:47:48 -0700

    Refactored the IGN_scraper to use multiprocessing
    For performance, refactored code to use multiprocessing
    instead of threading. The performance gain, however,
    was negligible, if anything.

--- src/IGN_scraper.py
+++ src/IGN_scraper.py
@@ -13,22 +13,18 @@
 # an end index. The script will fetch data from the start index
 # (0 by default) to the end index, inclusive. 
 #
+#
 # Written by Akshay Agrawal
 # July 28, 2013
 
+import argparse
+import sys
 import urllib2 				  # For URL opening
 from bs4 import BeautifulSoup # For HTML parsing
-import sys
-import argparse
-import threading 
+from multiprocessing import Process, Lock, Semaphore
 
 INDEX_INCREMENT = 50 # IGN's index increments by this fixed amount.
-MAX_NUM_THREADS = 8
-num_threads_allowed = threading.Semaphore(MAX_NUM_THREADS)
-outfile_lock = threading.Lock()
-urlopen_lock = threading.Lock()
-stderr_lock = threading.Lock()
-print_lock = threading.Lock()
+MAX_NUM_PROCESSES = 8
 
 # Entries for games are encoded as follows:
 # <div class="clear itemList-itemShort">
@@ -47,7 +43,8 @@ print_lock = threading.Lock()
 #				[Release Date] </div>
 # </div>
 # Note: The square brackets are not included in the HTML
-def parse_page(outfile, page_contents, score_flag, verbose):
+def parse_page(outfile, page_contents, score_flag, verbose,
+	outfile_lock, stderr_lock, print_lock):
 	htmltree = BeautifulSoup(page_contents) 
 	titles_links_platforms = htmltree.find_all("div", class_="item-title") 
 	publishers_scores_dates = htmltree.find_all("div", "grid_3")
@@ -87,6 +84,7 @@ def parse_page(outfile, page_contents, score_flag, verbose):
 		# Some games might not have a platform. See index 15750, Devil's Third.
 		else:
 			plat = "<unknown>"
+
 		# Obtain the <a> tag and extract the link from its href attribute. 
 		# Note that tag.contents[0] is a newline. 
 		link_tag = tag.contents[1]
@@ -97,7 +95,9 @@ def parse_page(outfile, page_contents, score_flag, verbose):
 		# The count-th entry in each of publishers_list, scores_list, and
 		# release_dates_list corresponds to the count-th tag
 		# in titles_links_platforms.
+		#
 		publisher = "\"" + publishers_list[count] + "\""
+
 		# Non-rated games are assigned a score of -1.
 		score = scores_list[count] if scores_list[count] != NO_SCORE else "-1"
 		release = "\"" + release_dates_list[count] + "\""
@@ -117,23 +117,23 @@ def parse_page(outfile, page_contents, score_flag, verbose):
 
 # Open the url, read its contents and send it for parsing.
 # If we fail to open the url, print an error message indicating as much.
-def open_url_and_parse(*args):
-	outfile, curr_url, score_flag, force_max, verbose = args
+def open_url_and_parse(outfile, curr_url, score_flag, force_max, verbose,
+	outfile_lock, urlopen_lock, stderr_lock, print_lock, num_workers):
+
+	# Error strings
 	IGN_ERROR_MESSAGE = "No Results"
 	page_contents = ""
-	urlfile = None
+
 	# As a heuristic, try to open the URL force_max times before giving up.
 	run = 0;
+	urlfile = None
 	while run < force_max:
-		if verbose:
-			print_lock.acquire()
-			print "Opening url: %s" % curr_url
-			print "%d threads are working" % (threading.active_count() - 1)
-			print_lock.release()
+		# Try to open the url
 		try:
 			urlopen_lock.acquire()
 			urlfile = urllib2.urlopen(curr_url)
 			urlopen_lock.release()
+		# Catch errors and attempt to reopen the url, if possible
 		except urllib2.HTTPError as e:
 			stderr_lock.acquire()
 			sys.stderr.write("HTTP error({0}): {1} with url {2}\n".format(
@@ -152,44 +152,68 @@ def open_url_and_parse(*args):
 			continue
 		# IGN_ERROR_MESSAGE is served a) at invalid indices but also b) on
 		# a request that fails. b) is unpredictable.
+		# Try to reopen the url, if possible
 		page_contents = urlfile.read();
 		if page_contents == "" or page_contents.startswith(IGN_ERROR_MESSAGE):	
 			run += 1
 			continue
 		break
+
+	# Clean-up 
 	if urlfile is not None:
 		urlfile.close()
+
+	# Did we succeed in opening the file?
 	if (run >= force_max):
 		stderr_lock.acquire()
 		sys.stderr.write("Failed to open url: %s\n" % curr_url)
 		stderr_lock.release()
-		num_threads_allowed.release()
+		num_workers.release()
 		return
-	parse_page(outfile, page_contents, score_flag, verbose)
-	num_threads_allowed.release()
 
-# For each page in the index, spawn a thread to open the page,
+	# We succeeded in opening the file. Send it to the parser.
+	parse_page(outfile, page_contents, score_flag, verbose,
+		outfile_lock, stderr_lock, print_lock)
+
+	# This process has now finished its work.
+	num_workers.release()
+
+# For each page in the index, spawn a process to open the page,
 # parse it, and publish the extracted data. 
 def launch_workers(outfile, start_index, end_index, score_flag, force, verbose):
 	BASE_URL = "http://www.ign.com/games/all-ajax?startIndex="
-	curr_index = start_index;
-	threads = []
+	
+	
+	# Synchronization Tools
+	num_workers = Semaphore(MAX_NUM_PROCESSES)
+	outfile_lock = Lock()
+	urlopen_lock = Lock()
+	stderr_lock = Lock()
+	print_lock = Lock()
+	
+	# Write the categories
 	if (outfile != None):
 		outfile.write("title,link,platform,publisher,score,date\n")
+
+	# Launch the workers
+	processes = []
+	curr_index = start_index;
 	while curr_index <= end_index:
 		curr_url = BASE_URL + str(curr_index)
-	 	parser_thread = threading.Thread(None, open_url_and_parse,
-	 		None, (outfile, curr_url, score_flag, force, verbose))
-	 	threads.append(parser_thread)
+	 	worker = Process(target=open_url_and_parse,
+	 		args=(outfile, curr_url, score_flag, force, verbose,
+	 			outfile_lock, urlopen_lock, stderr_lock, print_lock,
+	 			num_workers))
+	 	processes.append(worker)
 	 	if verbose:
 			print_lock.acquire()
 			print "Launching worker for url: %s" % curr_url
 			print_lock.release()
-	 	num_threads_allowed.acquire()
-	 	parser_thread.start()
+	 	num_workers.acquire()
+	 	worker.start()
 	 	curr_index += INDEX_INCREMENT; 
-	for t in threads:
-	 	t.join()
+	for p in processes:
+	 	p.join()
 	 	
 def main():
 	# args: [-h] [-si STARTINDEX] [-w OUTFILE] [-s] [-f MAX_ATTEMPTS] [-v] 
@@ -199,7 +223,7 @@ def main():
 		"game data.")
 	parser.add_argument("endindex", type=int, 
 		help="the index through which to parse. ")
-	parser.add_argument("-si", "--startindex", default=0, type=int, 
+	parser.add_argument("-st", "--startindex", default=0, type=int, 
 		help="the index at which to start. ")
 	parser.add_argument("-w", "--writetofile", type=str,
 		help="the file in which you would like the data to be saved")
@@ -224,7 +248,8 @@ def main():
 		"greater than the end index" %
 		(args.startindex, INDEX_INCREMENT))
 		return 1
-	# Vet the end index.
+	# Vet the end index. As of August 24, 2013, the maximum end index
+	# is 75450
 	if (args.endindex < 0) or (args.endindex % INDEX_INCREMENT != 0):
 		sys.stderr.write("invalid end index: %d\n" \
 			"end index must be non-negative and divisible by %d\n" %
